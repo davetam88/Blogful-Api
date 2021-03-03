4e5e66bd9554ae6d7563bdd3826931fd8957067a
David Tam <davetam88@gmail.com>
2021-03-03 07:43:51 -0500

    refactored array in articles.fixtures.js file

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -1,9 +1,7 @@
-// 18.15 - Now we should have a passing test when running npm test! Let's add an .only to the top level describe block so that we're only running this test file whilst working on it.
-
 const { expect } = require('chai')
 const knex = require('knex')
 const app = require('../src/app')
-
+const { makeArticlesArray } = require('./articles.fixtures')
 
 describe.only('Articles Endpoints', function () {
 
@@ -28,36 +26,7 @@ describe.only('Articles Endpoints', function () {
   afterEach('cleanup', () => db('blogful_articles').truncate())
 
   context('Given there are articles in the database', () => {
-    const testArticles = [
-      {
-        id: 1,
-        date_published: '2029-01-22T16:28:32.615Z',
-        title: 'First test post!',
-        style: 'How-to',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
-      },
-      {
-        id: 2,
-        date_published: '2100-05-22T16:28:32.615Z',
-        title: 'Second test post!',
-        style: 'News',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
-      },
-      {
-        id: 3,
-        date_published: '1919-12-22T16:28:32.615Z',
-        title: 'Third test post!',
-        style: 'Listicle',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
-      },
-      {
-        id: 4,
-        date_published: '1919-12-22T16:28:32.615Z',
-        title: 'Fourth test post!',
-        style: 'Story',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum molestiae accusamus veniam consectetur tempora, corporis obcaecati ad nisi asperiores tenetur, autem magnam. Iste, architecto obcaecati tenetur quidem voluptatum ipsa quam?'
-      },
-    ];
+    const testArticles = makeArticlesArray()
 
     // test 1  :  GET /articles all - work
     beforeEach('BEFOREEACH:  insert articles', () => {
